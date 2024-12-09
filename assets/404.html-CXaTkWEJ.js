8328d561f304a672a662c2a3dd12cf25c185b4c1
lanmei123 <97657314+lanmei123@users.noreply.github.com>
2024-12-09 15:59:13 +0000

    Deploying to blog_pages from @ lanmei123/blog@c95e79b689ea1271abbba5172e197b2029cc2c69 🚀

---
+++ assets/404.html-CXaTkWEJ.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,a,o as n}from"./app-jeEFIX8S.js";const l={};function r(s,t){return n(),o("div",null,t[0]||(t[0]=[a("p",null,"404 Not Found",-1)]))}const _=e(l,[["render",r],["__file","404.html.vue"]]),i=JSON.parse('{"path":"/404.html","title":"","lang":"zn-ch","frontmatter":{"layout":"NotFound"},"headers":[],"git":{},"filePathRelative":null}');export{_ as comp,i as data};
