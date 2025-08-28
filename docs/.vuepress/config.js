24fe5b2db8d328e92ddb8ae085f55e184fc95f6a
hmw <major@2025>
2025-08-28 10:21:59 +0800

    增加一个增肌饮食

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -45,7 +45,7 @@ export default defineUserConfig({
             },
             {
                 text: '其余杂谈',
-                children: ['/dailyTalk/good-code.md', {
+                children: ['/dailyTalk/good-code.md', '/dailyTalk/yinShi.md', {
                     text: "好文感悟",
                     children: ['/dailyTalk/minHabit.md']
                 }]
